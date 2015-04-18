8d748ef7283598d53509430ff353fab7c6475aed
angelo gimenez <gimenez.angelo@gmail.com>
2015-04-19 03:18:32 +0800

    modified:   feed.js

--- feed.js
+++ feed.js
@@ -1,54 +1,42 @@
+function twittlerInit() {
+    //make it a static var
+    var $body = $('.tweetbox');
 
-function twittlerInit(){
-  function updateTweets() {
-        var $body = $('.tweetbox');
-        var index = streams.home.length - 1;
-        while (index >= 0) {
-            var tweet = streams.home[index];
-            var $tweet = $('<div class = "tweet"> </div>');
-            var timenow = new Date().getTime();
-            var elapsedms = (timenow - tweet.created_at);
-            var elapsedS = Math.round(elapsedms / 1000);
-            $tweet.html('@<span class="user_class">' + tweet.user + '</span>' + '<br>' + tweet.message + '<br>' + '<span class="timestamp">' + elapsedS + " seconds ago" + '</span>');
-            $tweet.appendTo($body);
-            index -= 1;
-        }
+    function makeTweetElem(tweet) {
+        var $tweet = $('<div class = "tweet"> </div>');
+        var timenow = new Date().getTime();
+        var elapsedms = (timenow - tweet.created_at);
+        var elapsedS = Math.round(elapsedms / 1000);
+        $tweet.html('@<span class="user_class">' + tweet.user + '</span>' + '<br>' + tweet.message + '<br>' + '<span class="timestamp">' + elapsedS + " seconds ago" + '</span>');
+        return $tweet;
     }
 
-    $('.newtweets').on('click', function() {
-        var $body = $('.tweetbox');
-        $body.html('');
-        var index = streams.home.length - 1
-        while (index >= 0) {
-            var tweet = streams.home[index];
-            var $tweet = $('<div class = "tweet"> </div>');
-            var timenow = new Date().getTime();
-            var elapsedms = (timenow - tweet.created_at);
-            var elapsedS = Math.round(elapsedms / 1000);
-            $tweet.html('@<span class="user_class">' + tweet.user + '</span>' + '<br>' + tweet.message + '<br>' + '<span class="timestamp">' + elapsedS + " seconds ago" + '</span>');
-            $tweet.appendTo($body);
-            index -= 1;
-        }
-    });
-
-    $('.user_class').on('click', function() {
-        var username = $(this).text();
-        var $body = $('.tweetbox');
-        $body.html('');
-        var index = streams.home.length - 1
-        while (index >= 0) {
-            var tweet = streams.home[index];
-            if (username == tweet.user) {
-                var $tweet = $('<div class = "tweet"> </div>');
-                var ago = moment().startOf(tweet.created_at).fromNow();
-                $tweet.html('@<span class="user_class">' + tweet.user + '</span>' + '<br>' + tweet.message + '<br>' + '<span class="timestamp">' + elapsedS + " seconds ago" + '</span>');
-                $tweet.appendTo($body);
-                index -= 1;
+    function showUserTweets() {
+        var $tweetUser = $(this).text();
+        $body.find('.tweet').hide();
+        $body.find('.tweet').each(function() {
+            var user = $(this).find('span.user_class').text();
+            if (user == $tweetUser) {
+                $(this).show();
             }
-        }
-    });
+        });
+    }
 
+    function updateTweets() {
+
+        $body.html();
+        //loop through tweets
+        for (var i = streams.home.length - 1; i > 0; i--) {
+            var tweet = streams.home[i];
+            var $tweet = makeTweetElem(tweet);
+            $body.append($tweet);
+        }
+        $body.find('.user_class').on('click', showUserTweets);
+    }
+
+    $('.newtweets').on('click', updateTweets);
     //start the app, call update tweets function
+    window.setTimeout(updateTweets, 1500);
     updateTweets();
 }
 
