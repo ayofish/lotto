76251ed1e0e11f3977621121ee4f25130fe29ffd
gimenez.angelo@gmail.com <gimenez.angelo@gmail.com>
2015-05-14 09:59:12 +0800

    new file:   .yo-rc.json 	modified:   app/index.html 	modified:   app/scripts/app.js 	modified:   app/scripts/controllers/mynumbers.js

--- app/scripts/controllers/mynumbers.js
+++ app/scripts/controllers/mynumbers.js
@@ -8,8 +8,8 @@
  * Controller of the lottoApp
  */
 angular.module('lottoApp')
-  .controller('MynumbersCtrl', ['$scope', 'NumberChecker', function ($scope, numberChecker) {
-  	$scope.numbers = {
+    .controller('MynumbersCtrl', ['$scope', 'NumberChecker', function($scope, numberChecker) {
+        $scope.numbers = {
             winning: [{
                 val: null
             }, {
@@ -23,38 +23,40 @@ angular.module('lottoApp')
             }, {
                 val: null
             }],
-            mynumbers: [
-            ],
+            mynumbers: [],
             numMynumbers: 6
         };
-  	var addNumberSet = function(){
-  		var numberSet = [];
-  		for(var i =0;i< $scope.numbers.numMynumbers;i++){
-  			numberSet.push({
-                val: null
-            });
-  		}
-  		$scope.numbers.mynumbers.push(numberSet);
-  	};
-  	$scope.checkMyNumbers = function(){
-  		for(var i=0;i<$scope.numbers.mynumbers.length;i++){
-  			for(var x=0;x<$scope.numbers.mynumbers[i].length;x++){
-  				numberChecker.setNumbersToCheck(_.map($scope.numbers.mynumbers[i], function(obj){return obj.val;}));
-  				var matching = numberChecker.getMatching();
-  				for(var z=0;z<$scope.numbers.mynumbers[i].length;z++){
-  					if(matching.indexOf($scope.numbers.mynumbers[i][z].val) != -1){
-  						$scope.numbers.mynumbers[i][z].win = true;
-  					}
-  				}
-  			}
-  		}
-  		
-  	};
-  	$scope.addNumberSet = addNumberSet;
-  	$scope.addNumberSet();
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }]);
+        var addNumberSet = function() {
+            var numberSet = [];
+            for (var i = 0; i < $scope.numbers.numMynumbers; i++) {
+                numberSet.push({
+                    val: null
+                });
+            }
+            $scope.numbers.mynumbers.push(numberSet);
+        };
+        var checkMyNumbers = function() {
+            for (var i = 0; i < $scope.numbers.mynumbers.length; i++) {
+                for (var x = 0; x < $scope.numbers.mynumbers[i].length; x++) {
+                    numberChecker.setNumbersToCheck(_.map($scope.numbers.mynumbers[i], function(obj) {
+                        return obj.val;
+                    }));
+                    var matching = numberChecker.getMatching();
+                    for (var z = 0; z < $scope.numbers.mynumbers[i].length; z++) {
+                        if (matching.indexOf($scope.numbers.mynumbers[i][z].val) != -1) {
+                            $scope.numbers.mynumbers[i][z].win = true;
+                        }
+                    }
+                }
+            }
+
+        };
+        $scope.checkMyNumbers = checkMyNumbers;
+        $scope.addNumberSet = addNumberSet;
+        $scope.addNumberSet();
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    }]);
